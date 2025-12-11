5ecee545cc2afe7bd9f72b0bc0a9c3c8395b74da
Hemanth <hemanthrajcshy8055@gmail.com>
2025-12-12 00:40:27 +0530

    Questions

---
+++ src/com/hemanth/kunal/BinarySearch/questions/PeakIndexMountain.java
@@ -0,0 +1,21 @@
+package com.hemanth.kunal.BinarySearch.questions;
+
+public class PeakIndexMountain {
+    static void main(String[] args) {
+        System.out.println(findIndex(new int[]{1, 2, 3, 4, 2, 1}));
+    }
+
+    static int findIndex(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start; // or end since voth point to same highest num index
+    }
+}
